fc3740ecf85a8155ae4a9a582b253433ef9b32e7
Alex Chan <alyxchan87@gmail.com>
2022-10-21 23:39:13 -0700

    Fix cache headers for both succes and fail redirects

--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -4,7 +4,10 @@ import { prisma } from '../../../db/client'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query['slug']
-  debugger
+
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Cache-Control', 's-maxage=10000000, stale-while-revalidate')
 
   if (!slug || typeof slug !== 'string') {
     res.statusCode = 400
@@ -22,11 +25,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   if (!data || !data.url) {
     res.statusCode = 400
-
-    res.setHeader('Content-Type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Cache-Control', 's-maxage=10000000, stale-while-revalidate')
-
     res.send(JSON.stringify({ message: 'slug not found' }))
     return
   }
