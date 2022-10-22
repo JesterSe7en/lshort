09da045717c0275702e31a018ba9c7aff42dcd55
Alex Chan <alyxchan87@gmail.com>
2022-10-21 21:56:08 -0700

    Working redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,18 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
-import { nanoid } from 'nanoid'
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.nextUrl.pathname.startsWith('/api/get-url')) {
     console.log('returning early')
     return
   }
 
-  const random = nanoid()
-  const res = NextResponse.next()
-  res.cookies.set('poll-token', random, { sameSite: 'strict' })
+  const slug = req.nextUrl.pathname.split('/').pop()
 
-  return res
+  const data = await (
+    await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
+  ).json()
+
+  if (data?.url) {
+    return NextResponse.redirect(data.url)
+  }
 }
