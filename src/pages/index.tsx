fd9dc99ccb929d396276de43eaeabd297218b661
Alex Chan <alyxchan87@gmail.com>
2022-10-22 14:10:52 -0700

    Add _doc.tsx Update default font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-slate-700 py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-slate-700 py-2 font-sans">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
